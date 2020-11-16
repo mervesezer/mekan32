3cc05c099aa993aef411af1d107defbf42a63ee4
Merve Sezer <sezermerve2259@gmail.com>
2020-11-16 16:04:05 +0300

    İlk gönderim

---
+++ app_server/routes/index.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+var ctrlMain=require("../controllers/main");
+// get home page
+router.get("/",ctrlMain.index);
+module.exports = router;
